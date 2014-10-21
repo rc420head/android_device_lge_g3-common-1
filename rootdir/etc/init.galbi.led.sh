301462cab19ac9087f2996d470d4799212aae79c
Dan Pasanen <dan.pasanen@gmail.com>
2014-10-20 23:33:04 -0500

    init: also turn of led if at 100%
    Change-Id: If9ba37e4e561b2b825e1f9e1765786ae804e8e50

--- rootdir/etc/init.galbi.led.sh
+++ rootdir/etc/init.galbi.led.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-# Turn the led off if not plugged in
-if [ `cat /sys/class/power_supply/battery/charge_type` = "N/A" ]; then
+# Turn the led off if not plugged in or if already at 100%
+if [ `cat /sys/class/power_supply/battery/charge_type` = "N/A" ] || [ `cat /sys/class/power_supply/battery/capacity` == "100" ]; then
   echo 0 > /sys/devices/virtual/lg_rgb_led/use_patterns/setting
 fi
